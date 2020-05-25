9f2a003e38e9cb763ae2c9963104dd58f44bbebd
Luana-Catalina Pantiru <48496260+LuanaPantiru@users.noreply.github.com>
2020-05-25 19:36:03 +0300

    reorganization of methods

--- sala_spectacole/src/com/sala_spectacole/domain/Persoana.java
+++ sala_spectacole/src/com/sala_spectacole/domain/Persoana.java
@@ -3,10 +3,15 @@ package com.sala_spectacole.domain;
 public class Persoana {
     private String nume;
     private String prenume;
+    private String cnp;
 
-    public Persoana(String nume, String prenume) {
+    public Persoana() {
+    }
+
+    public Persoana(String nume, String prenume, String cnp) {
         this.nume = nume;
         this.prenume = prenume;
+        this.cnp = cnp;
     }
 
     public String getNume() {
@@ -17,4 +22,19 @@ public class Persoana {
         return prenume;
     }
 
+    public String getCnp() {
+        return cnp;
+    }
+
+    public void setNume(String nume) {
+        this.nume = nume;
+    }
+
+    public void setPrenume(String prenume) {
+        this.prenume = prenume;
+    }
+
+    public void setCnp(String cnp) {
+        this.cnp = cnp;
+    }
 }
